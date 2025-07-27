bb47c0e6a9fe2c0f0c5b755f262b03273a8c0683
Isshin <julien.michel1026@icloud.com>
2025-07-27 11:04:05 +0200

    [+] refactor style, [&] gonna add sign up page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -30,28 +30,43 @@ export default function LoginPage() {
         <div className="flex h-screen w-screen items-center justify-center bg-gray-50">
             <div className="z-10 w-full max-w-md overflow-hidden rounded-2xl border border-gray-100 shadow-xl">
                 <div className="flex flex-col items-center justify-center space-y-3 border-b border-gray-200 bg-white px-4 py-6 pt-8 text-center sm:px-16">
-                    <h3 className="text-xl font-semibold">Sign In</h3>
-                    <p className="text-sm text-gray-500">
-                        Use your email and password to sign in
+                    <h3 className="text-xl mb-2 tracking-[-.01em] font-semibold text-black">Log In</h3>
+                    <p className="text-sm tracking-[-.01em] text-black">
+                        Use your email and password to log in
                     </p>
                 </div>
                 <form onSubmit={handleLogin} className="flex flex-col space-y-4 bg-white px-4 py-8 sm:px-16">
-                    <input
-                        type="email"
-                        placeholder="Email"
-                        value={email}
-                        onChange={e => setEmail(e.target.value)}
-                        className="rounded-md border border-gray-300 px-4 py-2 text-sm"
-                        required
-                    />
-                    <input
-                        type="password"
-                        placeholder="Password"
-                        value={password}
-                        onChange={e => setPassword(e.target.value)}
-                        className="rounded-md border border-gray-300 px-4 py-2 text-sm"
-                        required
-                    />
+                    <div className="flex flex-col gap-4">
+                        <div className="flex flex-col">
+                            <label htmlFor="email" className="mb-1 text-sm font-medium text-gray-700">
+                                Email
+                            </label>
+                            <input
+                                id="email"
+                                type="email"
+                                placeholder="Your Email"
+                                value={email}
+                                onChange={e => setEmail(e.target.value)}
+                                className="rounded-md border text-black border-gray-300 px-4 py-2 text-sm"
+                                required
+                            />
+                        </div>
+
+                        <div className="flex flex-col">
+                            <label htmlFor="password" className="mb-1 text-sm font-medium text-gray-700">
+                                Password
+                            </label>
+                            <input
+                                id="password"
+                                type="password"
+                                placeholder="Your Password"
+                                value={password}
+                                onChange={e => setPassword(e.target.value)}
+                                className="rounded-md border text-black border-gray-300 px-4 py-2 text-sm"
+                                required
+                            />
+                        </div>
+                    </div>
                     {error && <p className="text-red-500 text-sm">{error}</p>}
                     <button
                         type="submit"
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,8 +33,8 @@ export default function Home() {
             Log In
           </Link>
           <Link
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex gap-2 items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="/signup"
+            className="rounded-full border border-solid border-black/[.08] bg-black transition-colors flex gap-2 items-center justify-center hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
+            href="/register"
           >
             <Image
               className="whitespace-normal"
